a76efd0e5039c461a5f762ebd2a5afd9b9c51500
Tommy Markopoulos <tommymarkops@gmail.com>
2023-07-06 16:29:20 -0400

    added rating over time data and edited search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -10,10 +10,10 @@ const SearchBar = ({ handleChange, collectAllData, form }) => {
         <input name="username" value={form.username} type="search" onChange={(e)=>handleChange(e)} placeholder="Enter username"></input>
         &nbsp; &nbsp; 
         <label>From: </label>
-        <input name="fromDate" value={form.fromDate} type="search" onChange={(e)=>handleChange(e)}></input>
+        <input placeholder="YYYY-MM" name="fromDate" value={form.fromDate} type="search" onChange={(e)=>handleChange(e)}></input>
         &nbsp; &nbsp; 
         <label>To: </label>
-        <input name="toDate" value={form.toDate} type="search" onChange={(e)=>handleChange(e)}></input>
+        <input placeholder="YYYY-MM" name="toDate" value={form.toDate} type="search" onChange={(e)=>handleChange(e)}></input>
         &nbsp;&nbsp;
         <button className='button-33' type="submit" onClick={(e)=>collectAllData(e)}>GetData</button>
       </form>
